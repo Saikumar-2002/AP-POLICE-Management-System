8ad9eb0865230dcbf80360b0de23240d9fa54d61
SAIKUMAR <saikumarkalva10@gmail.com>
2026-03-31 15:50:30 +0530

    added chnages in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3001;
 // Initialize database
 db.init();
 
+app.get('/', (req, res) => {
+  res.send('API is running successfully 🚀');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
