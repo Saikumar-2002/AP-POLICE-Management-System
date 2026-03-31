7d40f6f35b5a851facfd54b0fcfc3fc4b0119344
SAIKUMAR <saikumarkalva10@gmail.com>
2026-03-31 15:36:40 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://ap-police-management-system-1.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
